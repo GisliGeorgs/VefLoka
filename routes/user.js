e8954662dc6a57714f07ef6a568b7fa7c371d9cb
Gísli Georgsson <gisli08@hotmail.com>
2015-11-26 22:18:07 +0000

    Change pass validation

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ var router = express.Router();
 
 var users = require( '../lib/users' );
 var diary = require( '../lib/entries' );
+var validate = require( '../lib/validate' );
 
 var ensureLoggedIn = require( '../middleware/ensureLoggedIn' );
 
@@ -24,22 +25,28 @@ function userPost( req, res ){
     var oPass = req.body.oPass;
     var nPass = req.body.pass;
     var nPass2 = req.body.pass2;
-    if( nPass !== nPass2 ){
-        var data = { title: 'Notnadi', success: false };  
-        res.render( 'user', data );
-    }  
-    else{/*
-        users.changePass( user, oPass, nPass, function ( err, status ){
-            if( err ){
-                console.log( err );
-            }
-            var success = true;
-            if(  err || !success ){
-                success = false;
-            }
-            var data = { title: 'User', success: success, user: user };
-            res.render( 'user', data );
-        } );*/
+    var data = { title: 'Notandi', user: req.session.user };
+
+    var results = [];
+    var errors = [];
+    var isRequired = validate.isRequired( oPass ) && 
+                     validate.isRequired( nPass ) &&
+                     validate.isRequired( nPass2 );
+        
+    results.push( {
+        name: 'nýju lykilorðiðin eru ekki þau sömu',
+        result: isRequired && validate.isSame( nPass, nPass2 )
+    } );
+
+    for( var i = 0; i < results.length; i++ ){
+        console.log( results[i] );
+        if( !results[ i ].result ){
+            errors.push( results[ i ] );
+        } 
+    }                
+    
+    
+    if( !errors.length ){
         users.auth( username, oPass, function ( err, user ){
             if( user ){
                 users.changePass(username, nPass, function (err, status) {
@@ -50,14 +57,19 @@ function userPost( req, res ){
                     if (err || !success) {
                         success = false;
                     }
-                    var data = { title: 'Notandi', success: success, user: user };
+                    data.success = success;
                     res.render('user', data);
                 });
             }
             else{
-                var data = { title: 'Notnadi', success: false, user: req.session.user };
+                data.success = false;
                 res.render( 'user', data );
             }
-        } );
+        } );            
+    }
+    else{
+        data.errors = errors;
+        data.success = false;
+        res.render( 'user', data );
     }
 }
