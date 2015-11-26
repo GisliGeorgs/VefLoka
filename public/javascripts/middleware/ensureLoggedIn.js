5a666d1d87fe6afea4798004a02b0df0394a1231
Gísli Georgsson <gisli08@hotmail.com>
2015-11-26 22:19:22 +0000

    Henda dóti

--- public/javascripts/middleware/ensureLoggedIn.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function ensureLoggedIn(req, res, next) {
-  if (req.session.user) {
-    next(); // köllum í næsta middleware ef við höfum notanda
-  } else {
-    res.redirect('/login');
-  }
-};
