7f4500eca167e28b3a759428c4aad0fda2ef2526
Gísli Georgsson <gisli08@hotmail.com>
2015-11-26 21:51:57 +0000

    Register og login validation

--- routes/auth.js
+++ routes/auth.js
@@ -40,19 +40,47 @@ function createForm( req, res, next ){
 function createHandler( req, res, next ){
     var user = req.body.user;
     var pass = req.body.pass;
-
-    // hér vantar villumeðhöndlun
-    users.createUser( user, pass, function( err, status ){
-        if( err ){
-            console.error( err );
-        }
-        var success = true;
-        if( err || !status ){
-            success = false;
-        }
-        var data = { title: 'Nýskráning', post: true, success: success };
-        res.render( 'register', data );
+    
+    var results = [];
+    var errors = [];
+    
+    results.push( { 
+        name: 'Notendanafn',
+        value: user,
+        result: validate.isLength( user, 3 ) &&
+                validate.isRequired( user )
     } );
+    results.push( {
+        name: 'Lykilorð',
+        value: pass,
+        result: validate.isLength( pass, 5 ) &&
+                validate.isRequired( pass )        
+    } );
+    for( var i = 0; i < results.length; i++ ) {
+        if( !results[ i ].result ){
+            errors.push( results[ i ] );
+        }
+    }
+    var data = { title: 'Nýskráning' };
+    
+    if( errors.length ){        
+        // hér vantar villumeðhöndlun
+        users.createUser( user, pass, function( err, status ){
+            if( err ){
+                console.error( err );
+            }
+            var success = true;
+            if( err || !status ){
+                success = false;
+            }
+            var data = { title: 'Nýskráning', post: true, success: success };
+            res.render( 'register', data );
+        } );
+    }
+    else{
+        data.errors = errors;
+        res.render( 'register', data );
+    }
 }
 
 function redirectIfLoggedIn( req, res, next ){
@@ -77,25 +105,24 @@ function loginHandler( req, res, next ){
     var results = [];
     var errors = [];
     results.push( {
-        name: 'username',
+        name: 'Notendanafn',
         value: username,
         result: validate.isLength( username, 3 ) && 
                 validate.isRequired( username )
     } );
     results.push( {
-        name: 'password',
+        name: 'Lykilorð',
         value: password,
         result: validate.isLength( password, 5 ) &&
                 validate.isRequired( password )
     } );
     
-    for( var i = 0; i < 2; i++ ){
-        if( !results[i].result ){
-            errors.push( results[i] );
+    for( var i = 0; i < results.length; i++ ){
+        if( !results[ i ].result ){
+            errors.push( results[ i ] );
         } 
     }
     var data = { title: 'Innskráning', username: username };
-    data.errors = errors;
 
     if( errors.length ){
         users.auth(username, password, function (err, user) {
@@ -112,6 +139,7 @@ function loginHandler( req, res, next ){
         });        
     }
     else{
+        data.errors = errors;
         data.error = true;
         res.render( 'login', data );        
     }
@@ -122,142 +150,3 @@ function logout( req, res, next ){
         res.redirect( '/' );
     } );
 }
-
-/* Sjá /router/index.js
-function index( req, res ){    
-    if( req.session.user ){
-        var data = { title: 'Forsíða', user: req.session.user };
-        res.render( 'index', data );
-    }
-    else{
-        res.redirect( '/login' );
-    }
-}
-*/
-/* Sjá /router/diary.js
-function diaryPost( req, res ){
-    var userID = req.session.user.id;
-    var title = req.body.title;
-    var text = req.body.text;
-    var user = req.session.user;
-    //var data = [ userID, title, text, user ];
-    //console.log( data );
-    diary.addEntry( userID, title, text, function ( err, result ){
-        if( result ){
-            res.redirect( '/diary' );
-        }
-        else{
-            var data = { title: 'Dagbók', user: user, error: true };
-            res.render( 'diary', data );
-        }
-    } );
-}
-*/
-/* Sjá /router/diary.js
-function diaryGet( req, res ){
-    var user = req.session.user;
-    diary.getEntries( 10, user.id, function ( err, result ){
-        var data = { title: 'Dagbók', user: user, entries: result}
-        res.render( 'diary', data );
-    } );
-} */
-/* Sjá /routes/entry.js
-function entry( req, res ){
-    var user = req.session.user;
-    var id = req.query.id;
-    diary.getEntry( user.id, id, function ( err, result ){
-        if( result ){
-            var data = { title: id, user: user, id: id, entry: result[0] };
-            res.render( 'entry', data );
-        }
-        else{
-            res.redirect( '/diary' );
-        }
-    } );
-}*/
-/* Sjá /routes/entry.js
-function entryDelete( req, res ){
-    var user = req.session.user;
-    var id = req.query.id;
-    diary.deleteEntry( user.id, id, function ( err, result ){
-        if( result ){
-            console.log( 'Eyðing gagna tókst.' );
-            res.redirect( '/diary' );
-        }
-        else{
-            console.log( 'Villa kom upp við eyðingu gagnsins.' );
-            var data = { title: 'Færsla', errorDel: true };
-            res.redirect( '/entry?id=' + id, data );
-        }
-    } );
-}*/
-/* Sjá /routes/entry.js
-function entryUpdate( req, res ){
-    var user = req.session.user;
-    var id = req.query.id;
-    var title = req.body.title;
-    var text = req.body.text;
-    diary.updateEntry( user.id, id, title, text, function ( err, result ){
-        if( result ){
-            console.log( 'Breyting gagna tókst.' );
-            res.redirect( '/diary' );
-        }
-        else{            
-            var data = { title: 'Færsla', errorUp: true };
-            res.render( '/entry?id=' + id, data );
-        }
-    } );
-}*/
-/* Sjá /routes/user.js
-function userGet( req, res ){
-    var user = req.session.user;
-    var data = { title: 'User', user: user };
-    res.render( 'user', data );
-}
-*/
-/* Sjá /routes/user.js
-function userPost( req, res ){    
-    var username = req.session.user.username;
-    var oPass = req.body.oPass;
-    var nPass = req.body.pass;
-    var nPass2 = req.body.pass2;
-    if( nPass !== nPass2 ){
-        var data = { title: 'Notnadi', success: false };  
-        res.render( 'user', data );
-    }  
-    else{
-*/
-    /*
-        users.changePass( user, oPass, nPass, function ( err, status ){
-            if( err ){
-                console.log( err );
-            }
-            var success = true;
-            if(  err || !success ){
-                success = false;
-            }
-            var data = { title: 'User', success: success, user: user };
-            res.render( 'user', data );
-        } );*/
-/*
-        users.auth( username, oPass, function ( err, user ){
-            if( user ){
-                users.changePass( username, nPass, function ( err, status ){
-                    if( err ){
-                        console.log( err );
-                    }
-                    var success = true;
-                    if(  err || !success ){
-                        success = false;
-                    }
-                    var data = { title: 'Notandi', success: success, user: user };
-                    res.render( 'user', data );
-                } )
-            }
-            else{
-                var data = { title: 'Notnadi', success: false, user: req.session.user };
-                res.render( 'user', data );
-            }
-        } );
-    }
-}*/
