3e9f35f2c40b39171fb60e5c98ab30c65d78d054
Gísli Georgsson <gisli08@hotmail.com>
2015-11-28 13:13:01 +0000

    henda kommentuðu ónotuðum kóða

--- app.js
+++ app.js
@@ -45,14 +45,7 @@ app.use( session( {
 
 //////////////////
 app.locals.pretty = true;
-/*
-app.use( function (req, res, next) {
-  if (req.session.user) {
-    app.locals.user = req.session.user;
-  }
-  next();
-});
-*/
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
@@ -63,42 +56,8 @@ app.use( '/', entry );
 app.use( '/', index );
 app.use( '/', user );
 
-//app.use( '/', diary );
-
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-
-// catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-*/
-// error handlers
-
-// development error handler
-// will print stacktrace
-/*if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}*/
-
-// production error handler
-// no stacktraces leaked to user
-/*app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-*/
 
 module.exports = app;
